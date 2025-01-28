59a56ad0f0a7c38dfbde18db31ce6bbc117aa04e
dbenfon341 <dbenfon341@g.educaand.es>
2025-01-28 13:52:31 +0100

    Initial commit.

---
+++ src/classes/ConjuntoLibros.kt
@@ -0,0 +1,9 @@
+package classes
+
+//almacena un conjunto de libros (con un vector de un tamaño fijo). Se pueden añadir libros que no existan (siempre que haya espacio),
+//eliminar libros por título o autor, mostrar por pantalla los libros con la mayor y menor calificación y, por último, mostrar un contenido de todo el conjunto.
+class ConjuntoLibros {
+    val librosGuardados = Array<Libro?>(5) { null }
+
+
+}
---
+++ src/classes/Libro.kt
@@ -0,0 +1,10 @@
+package classes
+
+
+// título, el autor, el número de páginas y la calificación que le damos entre 0 y 10.
+class Libro() {
+
+    fun test() {
+        println("")
+    }
+}
